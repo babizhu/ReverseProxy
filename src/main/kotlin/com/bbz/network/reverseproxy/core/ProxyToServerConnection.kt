5a4787e6ced431535a172c82b5939d82fdc72328
liulaoye <liu_kun@msn.com>
2018-04-04 07:25:24 +0800

    完全是自己害自己，调试的时候故意修改的代码，居然没有改回来

--- src/main/kotlin/com/bbz/network/reverseproxy/core/ProxyToServerConnection.kt
+++ src/main/kotlin/com/bbz/network/reverseproxy/core/ProxyToServerConnection.kt
@@ -73,7 +73,7 @@ class ProxyToServerConnection(proxyServer: DefaultReverseProxyServer,
         channel.writeAndFlush(msg).addListener({
             if (it.isSuccess) {
                 // was able to flush out data, start to read the next chunk
-//                clientToProxyConnection.resumeRead()
+                clientToProxyConnection.resumeRead()
             } else {
                 exceptionOccur(it.cause())
             }
